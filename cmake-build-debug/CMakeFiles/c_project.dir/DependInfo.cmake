e956c72c10ecaa5f68c3f267ccade4722e30adde
Htin Linn Zaw <htinlinnzaw.dev@gmail.com>
2020-04-01 22:24:16 +0630

    Project First Initialization

---
+++ cmake-build-debug/CMakeFiles/c_project.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/root/ad5e01ce-e915-406e-b7d8-c911e13a3cd4/c-project/main.cpp" "/media/root/ad5e01ce-e915-406e-b7d8-c911e13a3cd4/c-project/cmake-build-debug/CMakeFiles/c_project.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/c_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_project.dir/main.cpp.o"
+  "c_project"
+  "c_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
